0418a75747385ca3444256eeefe8081c73a24a45
SaiAvinash-23 <saiavinashn@gwmail.gwu.edu>
2023-12-10 23:57:21 -0500

    Data Modeling

--- project.py
+++ project.py
@@ -614,12 +614,6 @@ print(crime_data['STREET'].value_counts())
 num_missing_STREET = crime_data['STREET'].isnull().sum()
 print("\nNumber of missing values in STREET: ", num_missing_STREET)
 
-#%%
-# Verify the operation
-print(crime_data['STREET'].value_counts())
-num_missing_STREET = crime_data['STREET'].isnull().sum()
-print("\nNumber of missing values in STREET: ", num_missing_STREET)
-
 
 # State of the 'STREET' Column
 # There are no missing values present in the 'STREET' column of the dataset.
