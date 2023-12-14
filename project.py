ce62d78a0f881af20f28bd09af153f2d2a3a3b3c
SaiAvinash-23 <saiavinashn@gwmail.gwu.edu>
2023-12-14 09:59:16 -0500

    Summary Paper & Final Code

--- project.py
+++ project.py
@@ -702,6 +702,27 @@ plt.legend(loc="lower right")
 plt.savefig('Log_ROC')
 plt.show()
 
+#%%
+# Evaluation
+print("Classification Report:\n", classification_report(y_test, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+print("ROC AUC Score:", roc_auc_score(y_test, y_pred_proba))
+
+#%%
+# ROC Curve
+fpr, tpr, thresholds = roc_curve(y_test, y_pred_proba)
+plt.figure()
+plt.plot(fpr, tpr, label='Logistic Regression (area = %0.2f)' % roc_auc_score(y_test, y_pred_proba))
+plt.plot([0, 1], [0, 1],'r--')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.savefig('Log_ROC')
+plt.show()
+
 # II) DECISION TREE (CLASSIFICATION TREE)
 
 #%%
